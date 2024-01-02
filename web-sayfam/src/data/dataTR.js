ceb4b99b47255f3e0fb28c5d78c2021c9ceac8a2
Aytekinozis <aytekinozis93@gmail.com>
2024-01-02 18:27:55 +0300

    trloc

--- web-sayfam/src/data/dataTR.js
+++ web-sayfam/src/data/dataTR.js
@@ -39,6 +39,7 @@ const dataTr = {
       lib3: "vercel",
       href: "",
       github: "",
+      image: "project1.png",
     },
     {
       projname: "Journey",
@@ -49,6 +50,7 @@ const dataTr = {
       lib3: "vercel",
       href: "",
       github: "",
+      image: "project2.png",
     },
   ],
   footer: {
