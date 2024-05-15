50babcfec243cade764f34a8deaa9d5c48b2519f
Aytekinozis <aytekinozis93@gmail.com>
2024-05-15 16:03:43 +0300

    en data changes

--- web-sayfam/src/data/dataEN.js
+++ web-sayfam/src/data/dataEN.js
@@ -1,6 +1,6 @@
 const dataEn = {
   header: {
-    name: "almilla",
+    name: "Aytekin",
     switchtoturkish: "Switch to Turkish",
   },
   topcomp: {
@@ -18,9 +18,9 @@ const dataEn = {
     city: "City of Residence",
     education: "Educational Status",
     role: "Preferred Role",
-    birthdate2: "24.03.1996",
-    city2: "Ankara",
-    education2: "Hacettepe Ünv. Biyoloji Lisans, 2016",
+    birthdate2: "23.02.1993",
+    city2: "İstanbul",
+    education2: "Uludağ university Environmental Engineering, 2020",
     role2: "Frontend, UI",
     about: "About Me",
     aboutfirst:
@@ -31,14 +31,14 @@ const dataEn = {
   project: "Projects",
   projects: [
     {
-      projname: "Workintech",
+      projname: "E*commerce",
       explaination:
-        "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+        "As the creator of this e-commerce website, I utilized React for its robust capabilities. React Router empowers me to seamlessly navigate between pages, while Axios ensures efficient handling of all API requests. React Redux serves as the backbone for managing global state effectively. For a visually appealing and user-friendly interface, I've incorporated Tailwind CSS and a selection of UI libraries. Additionally, React Hook Form streamlines form management and validation. The website empowers users to sign up, sign in, browse products, and make purchases with ease.",
       lib1: "react",
       lib2: "redux",
       lib3: "vercel",
-      href: "",
-      github: "",
+      href: "https://e-commerce-web-project.vercel.app",
+      github: "https://github.com/Aytekinozis/e-commerce-web-project",
       image: "project1.png",
     },
     {
